d3029d72f92c07ee29ab5fc36a5ce13b23f36fbb
nfal001 <abdurahman.nfal@gmail.com>
2024-05-05 21:08:33 +0700

    ProductPhotosController checkpoint

--- crud-naufal/app/Http/Controllers/ProductPhotosController.php
+++ crud-naufal/app/Http/Controllers/ProductPhotosController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\ProductPhotos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductPhotosController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index($productId)
     {
-        //
+        $product = Product::find($productId)->load('photos');
+
+        return response()->json([
+            'success' => 1,
+            'message' => 'Successfully Fetch Product Photos',
+            'data' => $product->photos
+        ]);
     }
 
     /**
@@ -26,9 +34,45 @@ class ProductPhotosController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, $productId)
     {
-        //
+        $product = Product::find($productId);
+        if ($product) {
+
+            $request->validate([
+                'photos' => 'required|array',
+                'photos.*' => 'image' 
+            ]);
+
+            $photos = $request->file('photos');
+
+            foreach ($photos as $photo) {
+                $fileUuid = Str::uuid();
+                $photosFilepath = 'products/photos';
+        
+                $fileExtensions = $photo->extension();
+                        
+                $filename =  $fileUuid . '.' . $fileExtensions;
+                $photo->storeAs($photosFilepath, $filename);
+
+                $product->photos()->create([
+                    'photo_url' => $photosFilepath . '/' . $filename
+                ]);
+            }
+
+            // dd("OK", $product->load('photos')->photos->toArray());
+
+            return response()->json([
+                'success' => 1,
+                'message' => 'Photos Added to Product'
+            ]);
+        }
+
+        return response()->json([
+            'success' => 0,
+            'message' => 'Product Not Found'
+        ], 404);
+        dd("NOT OK");
     }
 
     /**
